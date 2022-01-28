bd26565f3886cdbff03fa0c4399d7105d50309e8
r3card0 <ricardo.rdz@pm.me>
2022-01-27 22:17:29 -0600

    Variables decription

---
+++ JS-Basico/variables.js
@@ -0,0 +1,27 @@
+// sintaxis para crear una variable
+// el ; indica que se termino un sentencia
+var nombre = "Capitán tomate";
+
+// estados de una variable: declarar e inicializar
+// declarar
+var edad;
+// inicializar -> indica que ya tiene un valor
+edad = 50
+
+// variables de arrays
+// array es un conjunto de elementos
+var elements = ["computer", "phone", "car"];
+
+// para llamar por elemento
+elements[2];
+
+// variables de tipo objeto
+// variable compleja: guarda varios tipos de datos
+
+var objeto = {
+    nombre: "Bruce",
+    edad: 30
+}
+
+// llamar el contenido de la variable objeto
+objeto;
