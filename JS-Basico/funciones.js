76402e2ad566f07242269c948f677d82d2421367
r3card0 <ricardo.rdz@pm.me>
2022-01-27 23:12:53 -0600

    Functions syntax

---
+++ JS-Basico/funciones.js
@@ -0,0 +1,16 @@
+// funciones declarativas
+
+function gomagoma(){
+    return 3;
+}
+
+gomagoma(); // asi se llama una función
+
+
+// funciones de Expresión
+
+var gomagoma = function(){
+    return a + b;
+}
+
+gomagoma();
