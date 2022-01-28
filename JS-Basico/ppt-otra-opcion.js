85d187117c6c4b75bf20b3a693590faa3919ce2b
r3card0 <ricardo.rdz@pm.me>
2022-01-28 12:33:16 -0600

    An example of a simple code

---
+++ JS-Basico/ppt-otra-opcion.js
@@ -0,0 +1,24 @@
+// como se ingresa o se eije el valor??
+
+var jugador;
+var computador;
+
+if (jugador == "tijeras"  && computador == "papel") 
+{
+    console.log("Ganaste");
+}
+else if (jugador == "Piedra"  && computador == "tijeras") 
+{
+    console.log("Ganaste");
+}
+else if (jugador == "papel"  && computador == "piedra") 
+{
+    console.log("Ganaste");
+}
+else if (jugador == computador ) 
+{
+    console.log("empate");
+}
+else {
+    console.log("Perdiste")
+}
