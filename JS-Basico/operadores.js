6d156475a36632fee12b629d8411726af90c1519
r3card0 <ricardo.rdz@pm.me>
2022-01-28 09:54:17 -0600

    examples of operators

---
+++ JS-Basico/operadores.js
@@ -0,0 +1,37 @@
+// binary 
+
+3 + 2;
+50 - 10;
+10 * 20;
+20 / 2;
+
+// concatenate strings
+
+"Bruce" + "Wayne"
+
+// unity operators
+
+!false
+// true
+
+// asignación =, 
+
+var a = 1;
+
+// comparison ==, ===, <, >, <=, >=
+3 == "3"; // true
+3 === "3" // false
+5 > 3;
+5 >= 6;
+5 < 4;
+5 <= 6;
+
+// logicals &&, ||
+a && b // and
+a || b // or
+
+// ++, =+
+var edad = 40 
+edad ++  ;   // incrementar por 1
+edad += 2;  // incrementar por 2
+
---
+++ Temas/09_operadores.md
@@ -0,0 +1,13 @@
+# Operadores
+
+Tipos de operadores:
+
+| Operador    |       símbolo(s)      |
+|-------------|-----------------------|
+| Aritméticos | +, -, /, //, *        |
+| Asignación  | =, !=                 |
+| Comparación | ==, ===, <, >, <=, >= |
+| Lógicos     | && (and), II (or), not|
+| variables   | ++, +=                |
+| Otros       | !false, !true         |
+
