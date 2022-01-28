db68b13f1184a5e839a9a7c84645d46e51cc469b
r3card0 <ricardo.rdz@pm.me>
2022-01-28 12:32:38 -0600

    The program shows who won the game

--- JS-Basico/piedra-papel-tijera.js
+++ JS-Basico/piedra-papel-tijera.js
@@ -10,11 +10,35 @@ var persona_1 = "piedra";
 var persona_2 = "papel";
 
 if (persona_1 == piedra && persona_2 == papel || persona_1 == papel && persona_2 == piedra) {
-    console.log(papel_gana_piedra + ": Papel le gana a piedra ✋ ")
+    console.log(papel_gana_piedra + ": Papel le gana a piedra ✋ ");
+    var ganador = function(){
+        if (persona_1 == papel) {
+            console.log("Ganó: persona_1" );
+        } else if (persona_2 == papel) {
+            console.log("Ganó: persona_2" );
+        }
+    }
+    console.log(ganador())
 } else if(persona_1 == tijeras && persona_2 == papel || persona_1 == papel && persona_2 == tijeras) {
     console.log(tijeras_gana_papel + ": Tijeras le gana a papel ✌️ ")
+    var ganador = function(){
+        if (persona_1 == tijeras) {
+            console.log("Ganó: persona_1" );
+        } else if (persona_2 == tijeras) {
+            console.log("Ganó: persona_2" );
+        }
+    }
+    console.log(ganador())
 } else if (persona_1 == piedra && persona_2 == tijeras || persona_1 == tijeras && persona_2 == piedra) {
     console.log(piedra_gana_tijeras + ": Piedra le gana a tijeras 👊 ")
+    var ganador = function(){
+        if (persona_1 == piedra) {
+            console.log("Ganó: persona_1" );
+        } else if (persona_2 == piedra) {
+            console.log("Ganó: persona_2" );
+        }
+    }
+    console.log(ganador())
 } else {
     console.log("Empate, intenten de nuevo 😜 ")
 }
